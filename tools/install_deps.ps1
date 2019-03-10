4c4c211cc8bf90fa93a286398a00711628443773
Adam <almcchesney@gmail.com>
2019-03-09 22:55:04 -0600

    more deps

--- tools/install_deps.ps1
+++ tools/install_deps.ps1
@@ -1,3 +1,8 @@
 
+
+sudo apt install wget -y
+
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
+
+sudo apt install dotnet-sdk-2.1 -y
