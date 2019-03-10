9d3aa3717c1da062f7ae64f451d49632e5ffc1a9
Adam <almcchesney@gmail.com>
2019-03-09 19:36:03 -0600

    added directory creation

--- tools/compile.ps1
+++ tools/compile.ps1
@@ -1,10 +1,13 @@
-#Perform Setup
+#Region Perform Setup
 Install-Module PS-AutoApi -Force
 Import-Module PS-AutoApi
+
+mkdir build
 
 & $PSScriptRoot/setup.j2.ps1
 
 Clear-Routes
+#EndRegion
 
 Write-Host "Importing Paths"
 . ./src/main.ps1
@@ -12,6 +15,5 @@ Write-Host "Importing Paths"
 @{ Routes= Get-RegisteredRoutes} |
     ConvertTo-Json |
     j2 --format=json serverless-template.yml -o build/serverless.yml
-
 
 If($Error.Count -gt 0){ Exit 1 }
